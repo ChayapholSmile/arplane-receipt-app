c45b0b1d6644927c54f55eea01217f431998b2b5
ChayapholSmile <151313831+ChayapholSmile@users.noreply.github.com>
2024-04-19 17:47:56 +0700

    Create thanksfordownload-b-th-th.cmd

---
+++ thanksfordownload-b-th-th.cmd
@@ -0,0 +1,7 @@
+## Thank you for download Arplane software!!
+Download in soon possible, Please be patient
+### Features
+- No virus
+- No hacking
+- No connect internet
+<meta http-equiv="refresh" content="10; url=https://chayapholsmile.github.io/arplane-recipt-app/B-th-th.cmd">
